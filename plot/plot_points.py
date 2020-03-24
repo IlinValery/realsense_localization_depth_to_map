20df4cd7a5c9dc61359b6a4c7fa4849bcf06e39d
Valery Ilin <valery.ilin@skoltech.ru>
2020-03-24 11:25:39 +0300

    plot files template

---
+++ plot/plot_points.py
@@ -0,0 +1,9 @@
+# TODO: Valery 3dpoints visualisation
+
+
+def plot_points(points=None, trajectories=None):
+    if trajectories is not None:
+        # TODO: plot initial
+        for trajectory in trajectories:
+            # TODO: plot here
+            pass
---
+++ plot/plot_trajectory.py
@@ -0,0 +1,9 @@
+# TODO: Stepan trajectory visualisation
+
+
+def plot_trajectory(trajectories=None):
+    if trajectories is not None:
+        # TODO: plot initial
+        for trajectory in trajectories:
+            # TODO: plot here
+            pass
