5c28420fb1483217be800f827c9c9425fc234fe0
Stepan <Stepan.Perminov@skoltech.ru>
2020-03-24 22:45:09 +0300

    Trajectory plot works

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+#!/usr/bin/env python3.7
+
+import sys
+sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages') # in order to import cv2 under python3
 import cv2
+sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages') # append back in order to import rospy
 from sensors_wrappers.d435_sensor import D435Sensor
 from sensors_wrappers.t265_sensor import T265Sensor
+from plot.plot_trajectory import plot_trajectory
+import matplotlib.pyplot as plt
 
 is_device = False
 is_write_to_bag = False
 pose_number = 0
+show_plot_trajectory = True
 
 if __name__ == "__main__":
     if is_device:
@@ -24,6 +32,15 @@ if __name__ == "__main__":
     T265.start_sensor()
     D435.start_sensor()
 
+    transformation_matrix_set = []
+
+    fig = None
+    ax = None
+    if show_plot_trajectory:
+        fig = plt.figure(1)
+        ax = fig.add_subplot(111, projection='3d')
+        plt.ion()
+
     try:
         while True:
             # TODO: all manupulations with data here
@@ -33,8 +50,8 @@ if __name__ == "__main__":
                 print('\nframeset435 gtab time', color_frame.get_timestamp())
                 print('pose265 grab time    ', pose265.get_timestamp())
 
-                # transformation_matrix = T265.get_transformation()
-                # print('transformation_matrix',transformation_matrix)
+                transformation_matrix = T265.get_transformation()
+                print('transformation_matrix',transformation_matrix)
 
                 color_image, depth_image = D435.get_images()
                 cv2.imshow('D435 RGB Frame', color_image)
@@ -42,9 +59,15 @@ if __name__ == "__main__":
                 cv2.imshow('D435 Depth Frame', depth_image)
                 cv2.waitKey(1)
 
+            if show_plot_trajectory:
+                transformation_matrix_set.append(T265.get_transformation())
+                plot_trajectory(transformation_matrix_set, ax, trajectories=[1])
+
     except KeyboardInterrupt:
         D435.stop_sensor()
         T265.stop_sensor()
+        if show_plot_trajectory:
+            plt.show(block=True)
     finally:
         D435.stop_sensor()
         T265.stop_sensor()
