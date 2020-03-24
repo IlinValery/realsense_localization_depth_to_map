63185ef134b8dd728ae2deebc4fef5f56d7d975b
negvet <e.tsykunov@gmail.com>
2020-03-24 21:37:33 +0300

    sync record without color

--- sensors_wrappers/d435_sensor.py
+++ sensors_wrappers/d435_sensor.py
@@ -14,14 +14,14 @@ class D435Sensor(BaseSensor, BaseSubject):
         # Initialization of D435 sensor
         super(D435Sensor, self).__init__(is_device, source_name)
         self.cfg.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 30)
-        self.cfg.enable_stream(rs.stream.color, 848, 480, rs.format.bgr8, 30)
+        # self.cfg.enable_stream(rs.stream.color, 848, 480, rs.format.bgr8, 30)
 
         # initialize observers
         self._observers: List[BaseObserver] = [] # pattern observer in common
 
         # TODO: insert initial conditions here:
         self.frameset = None
-        self.color_frame = None
+        # self.color_frame = None
         self.depth_frame = None
         self.color_image = None
         self.depth_image = None
@@ -44,19 +44,19 @@ class D435Sensor(BaseSensor, BaseSubject):
 
     def process_frameset(self):
         # TODO extract necessary data here:
-        self.color_frame = self.frameset.get_color_frame()
+        # self.color_frame = self.frameset.get_color_frame()
         self.depth_frame = self.frameset.get_depth_frame()
-        self.color_image = np.asanyarray(self.color_frame.get_data())
+        # self.color_image = np.asanyarray(self.color_frame.get_data())
         self.depth_image = np.asanyarray(self.depth_frame.get_data())
 
     def get_frameset(self):
         return self.frameset
 
     # TODO: get functions here:
-    def get_images(self):
-        return self.color_image, self.depth_image
+    def get_image(self):
+        return self.depth_image
 
-    def get_frames(self):
-        return self.color_frame, self.depth_frame
+    def get_frame(self):
+        return self.depth_frame
 
 
